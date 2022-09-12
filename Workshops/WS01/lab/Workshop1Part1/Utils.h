e2be4ee4b63eed6fd1a046dfcbefe0993180559c
Carmen Lau <clau51@myseneca.ca>
2022-09-12 19:02:20 -0400

    updated name

--- Workshops/WS01/lab/Workshop1Part1/Utils.h
+++ Workshops/WS01/lab/Workshop1Part1/Utils.h
@@ -1,3 +1,12 @@
+/*
+Name: Carmen Lau
+Email: clau51@myseneca.ca
+Student ID: 166689216
+Date: Sep 12, 2022
+I have done all the coding by myself and only copied the code that
+my professor provided to complete my workshops and assignments.
+*/
+
 #ifndef SDDS_UTILS_H
 #define SDDS_UTILS_H
 
