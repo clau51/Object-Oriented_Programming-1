88709c0d703c2a0ee4e51e8a4ee60f3777aa84dd
Carmen Lau <clau51@myseneca.ca>
2022-09-30 15:17:02 -0400

    final copy

--- Workshops/WS03/w3p1/CC.cpp
+++ Workshops/WS03/w3p1/CC.cpp
@@ -1,38 +1,44 @@
+//Name: Carmen Lau
+//Student ID: 166689216
+//Email: clau51@myseneca.ca
+//Date: September 30, 2022
+//Section: NBB
+//I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+
 #include <iostream>
 #include "CC.h"
 #include "Utils.h"
 
 using namespace std;
-using namespace sdds;
 
 namespace sdds
 {
+	const int MAX_NAME_LEN = 70;
+
+	//Validate credit card information
 	bool CC::validate(const char* name,
 		unsigned long long cardNo,
 		short cvv,
 		short expMon,
 		short expYear)const
 	{
-		long long minCard = 4000000000000000;
-		long long maxCard = 4099999999999999;
+		unsigned long long minCard = 4000000000000000;
+		unsigned long long maxCard = 4099999999999999;
 		short minMonth = 1;
 		short maxMonth = 12;
+		short minYear = 22;
+		short maxYear = 32;
 		short minCvv = 100;
 		short maxCvv = 999;
-		int ok = 0;
 
-		/*if (name == nullptr || strlen(name) <= 2) ok++;
-		if (cardNo < minCard || cardNo > maxCard) ok++;
-		if (cvv < minCvv || cvv > maxCvv) ok++;
-		if (expMon < minMonth || expMon > maxMonth) ok++;
-		if (expYear < 22 || expYear > 32) ok++;
-		return ok == 0;*/
-		return (name != nullptr && strlen(name) > 2) &&
+		return (name != nullptr && sdds::strlen(name) > 2) &&
 			(cardNo >= minCard && cardNo <= maxCard) &&
 			(cvv >= minCvv && cvv <= maxCvv) &&
-			(expMon >= minMonth && expMon <= maxMonth);
+			(expMon >= minMonth && expMon <= maxMonth) &&
+			(expYear >= minYear && expYear <= maxYear);
 	}
 
+	//Print 16-digit number in a set of 4-digit numbers seperated by spaces
 	void CC::prnNumber()const
 	{
 		cout.width(4);
@@ -47,6 +53,7 @@ namespace sdds
 		cout.fill(' ');
 	}
 
+	//Sets object to safe empty state
 	void CC::set()
 	{
 		m_name = nullptr;
@@ -56,17 +63,20 @@ namespace sdds
 		m_creditNo = 0;
 	}
 
+	//Deallocate cardholder's name and sets object to safe empty state
 	void CC::cleanUp()
 	{
 		delete[] m_name;
 		set();
 	}
 
+	//Returns true if object is in safe empty state
 	bool CC::isEmpty() const
 	{
 		return m_name == nullptr;
 	}
 
+	//Sets object to values if data is valid
 	void CC::set(const char* cc_name,
 		unsigned long long cc_no,
 		short m_cvv,
@@ -76,8 +86,8 @@ namespace sdds
 		cleanUp();
 		if (validate(cc_name, cc_no, m_cvv, m_expMon, m_expYear))
 		{
-			m_name = new char[strlen(cc_name) + 1];
-			strcpy(m_name, cc_name);
+			m_name = new char[sdds::strlen(cc_name) + 1];
+			sdds::strcpy(m_name, cc_name);
 			m_creditNo = cc_no;
 			CC::m_cvv = m_cvv;
 			m_expMonth = m_expMon;
@@ -85,9 +95,10 @@ namespace sdds
 		}
 	}
 
+	//Read values from console if data is valid and return true if values are stored in object successfully
 	bool CC::read()
 	{
-		char tempName[71 + 1];
+		char tempName[MAX_NAME_LEN + 1];
 		short tempCvv;
 		short tempMonth;
 		short tempYear;
@@ -95,7 +106,7 @@ namespace sdds
 
 		cleanUp();
 		cout << "Card holder name: ";
-		cin.getline(tempName, 71 + 1); //read up to 71 characters (discards new line)
+		cin.getline(tempName, MAX_NAME_LEN + 1);
 		if (!cin.fail()) cout << "Credit card number: ";
 		cin >> tempCredit;
 		if (!cin.fail()) cout << "Card Verification Value (CVV): ";
@@ -104,19 +115,14 @@ namespace sdds
 		cin >> tempMonth;
 		cin.ignore(1);
 		cin >> tempYear;
-		if (!cin.fail())
-		{
-			set(tempName, tempCredit, tempCvv, tempMonth, tempYear);
-		}
-		else
-		{
-			cin.clear();
-		}
+		if (!cin.fail()) set(tempName, tempCredit, tempCvv, tempMonth, tempYear);
+		else cin.clear();
 		cin.ignore(2000, '\n');
 
 		return isEmpty() == false;
 	}
 
+	//Display formatted credit card information
 	void CC::display(int row) const
 	{
 		if (isEmpty()) cout << "Invalid Credit Card Record" << endl;
@@ -124,6 +130,7 @@ namespace sdds
 		{
 			if (row > 0)
 			{
+				//Print row number
 				cout << "| ";
 				cout.width(3);
 				cout.setf(ios::right);
@@ -131,12 +138,14 @@ namespace sdds
 				cout.unsetf(ios::right);
 				cout << " | ";
 
-				if ((strlen(m_name)) > 30)
+				//If name is longer than 30 characters, print only the first 30 characters
+				if ((sdds::strlen(m_name)) > 30)
 				{
-					char tempName[30 + 1]{};
-					strcpy(tempName, m_name, 30);
+					char tempName[30 + 1];
+					sdds::strcpy(tempName, m_name, 30);
 					cout << tempName;
 				}
+				//Else print the whole name
 				else
 				{
 					cout.width(30);
@@ -144,15 +153,23 @@ namespace sdds
 					cout << m_name;
 				}
 				cout.unsetf(ios::left);
+				
+				//Print credit card number
 				cout << " | ";
 				prnNumber();
 				cout << " | ";
+
+				//Print CVV code
 				cout << m_cvv;
 				cout << " | ";
+				
+				//Print expiry month
 				cout.width(2);
 				cout.setf(ios::right);
 				cout << m_expMonth;
 				cout.unsetf(ios::right);
+
+				//Print expiry year
 				cout << "/" << m_expYear << " |" << endl;
 			}
 			else if (row <= 0)
--- Workshops/WS03/w3p1/CC.h
+++ Workshops/WS03/w3p1/CC.h
@@ -1,3 +1,10 @@
+//Name: Carmen Lau
+//Student ID: 166689216
+//Email: clau51@myseneca.ca
+//Date: September 30, 2022
+//Section: NBB
+//I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+
 #ifndef SDDS_CC_H_
 #define SDDS_CC_H_
 namespace sdds
@@ -11,25 +18,38 @@ namespace sdds
 		short m_expYear;
 		unsigned long long m_creditNo;
 
+		//Validate credit card information
 		bool validate(const char* name,
 			unsigned long long cardNo,
 			short cvv,
 			short expMon,
 			short expYear)const;
 
+		//Print 16-digit number in a set of 4-digit numbers seperated by spaces
 		void prnNumber()const;
+		
 	public:
+		//Sets object to safe empty state
 		void set();
+		
+		//Deallocate cardholder's name and sets object to safe empty state
 		void cleanUp();
+		
+		//Returns true if object is in safe empty state
 		bool isEmpty() const;
+
+		//Sets object to values if data is valid
 		void set(const char* cc_name,
 			unsigned long long cc_no,
 			short m_cvv,
 			short m_expMon,
 			short m_expYear);
-		bool read();
-		void display(int row = 0) const;
 
+		//Read values from console if data is valid and return true if values are stored in object successfully
+		bool read();
+		
+		//Display formatted credit card information
+		void display(int row = 0) const;
 	};
 }
 #endif // !SDDS_CC_H_
