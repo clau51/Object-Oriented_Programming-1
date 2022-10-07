b4723dc8e6435c268839ea4d8def6c06e9e34be7
Carmen Lau <clau51@myseneca.ca>
2022-10-07 18:14:29 -0400

    final copy - comments - adjustments

--- Workshops/WS04/p1/Seat.cpp
+++ Workshops/WS04/p1/Seat.cpp
@@ -1,5 +1,11 @@
-#define _CRT_SECURE_NO_WARNINGS
+//Name: Carmen Lau
+//Student ID: 166689216
+//Email: clau51@myseneca.ca
+//Date: October 7, 2022
+//Section: NBB
+//I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
 
+#define _CRT_SECURE_NO_WARNINGS
 #include "Seat.h"
 #include <cstring>
 #include <fstream>
@@ -7,14 +13,23 @@ using namespace std;
 
 namespace sdds {
 	const int MAX_CHARS = 40;
+	const int MAX_NAME_LEN = 70;
+	const int MIN_BUSINESS = 1;
+	const int MAX_BUSINESS = 4;
+	const int MIN_ECONPLUS = 7;
+	const int MAX_ECONPLUS = 15;
+	const int MIN_ECON = 20;
+	const int MAX_ECON = 38;
 
+	//Validate correct row numbers and seating letters
 	bool Seat::validate(int row, int letter)const
 	{
-		return (((row >= 1 && row <= 4) && (letter == 'A' || letter == 'B' || letter == 'E' || letter == 'F')) ||
-			((row >= 7 && row <= 15) && (letter >= 'A' && letter <= 'F')) ||
-			((row >= 20 && row <= 38) && (letter >= 'A' && letter <= 'F')));
+		return (((row >= MIN_BUSINESS && row <= MAX_BUSINESS) && (letter == 'A' || letter == 'B' || letter == 'E' || letter == 'F')) ||
+			((row >= MIN_ECONPLUS && row <= MAX_ECONPLUS) && (letter >= 'A' && letter <= 'F')) ||
+			((row >= MIN_ECON && row <= MAX_ECON) && (letter >= 'A' && letter <= 'F')));
 	}
 
+	//Check if string is valid and if it is, copy string to name
 	Seat& Seat::alAndCp(const char* str) //deallocate before calling
 	{
 		m_name = nullptr;
@@ -27,6 +42,7 @@ namespace sdds {
 		return *this;
 	}
 
+	//Reset object to safe empty state
 	Seat& Seat::reset()
 	{
 		delete[] m_name;
@@ -37,40 +53,51 @@ namespace sdds {
 		return *this;
 	}
 
+	//Check if object is in empty state
 	bool Seat::isEmpty()const
 	{
 		return m_name == nullptr;
 	}
 
+	//Check if seats are assigned and valid
 	bool Seat::assigned()const
 	{
 		return validate(m_row, m_letter);
 	}
 	
+	//Default constructor - set object to safe empty state
 	Seat::Seat()
 	{
 		m_name = nullptr;
 		reset();
 	}
 	
+	//Constructor - deallocate memory and copy string to name
 	Seat::Seat(const char* passengerName)
 	{
+		m_name = nullptr;
+		delete[] m_name;
 		alAndCp(passengerName);
 		m_row = 0;
 		m_letter = 0;
 	}
 
+	//Constructor - deallocate memory and copy string to name, then set row and letter
 	Seat::Seat(const char* passengerName, int row, char letter)
 	{
+		m_name = nullptr;
+		delete[] m_name;
 		alAndCp(passengerName);
 		set(row, letter);
 	}
 	
+	//Destructor - deallocate memory
 	Seat::~Seat()
 	{
 		delete[] m_name;
 	}
 
+	//Set row and letter (validate first)
 	Seat& Seat::set(int row, char letter)
 	{
 		if (validate(row, letter)) 
@@ -87,78 +114,86 @@ namespace sdds {
 		return *this;
 	}
 
+	//Get row
 	int Seat::row()const
 	{
 		return m_row;
 	}
 
+	//Get letter
 	char Seat::letter()const
 	{
 		return m_letter;
 	}
 	
+	//Get passenger name
 	const char* Seat::passenger()const
 	{
 		return m_name;
 	}
 
-	ostream& Seat::display(ostream& coutRed)const
+	//Display passenger and seat information in formatted manner
+	ostream& Seat::display(ostream& coutRef)const
 	{
 		char tempString[MAX_CHARS + 1];
 
 		if (isEmpty())
 		{
-			coutRed << "Invalid Seat!";
+			coutRef << "Invalid Seat!";
 		}
 		else if (validate(m_row, m_letter))
 		{
-			coutRed.width(40);
-			coutRed.setf(ios::left);
-			coutRed.fill('.');
-			if (strlen(m_name) > 40)
+			coutRef.width(MAX_CHARS);
+			coutRef.setf(ios::left);
+			coutRef.fill('.');
+			if (strlen(m_name) > MAX_CHARS)
 			{
 				strncpy(tempString, m_name, MAX_CHARS);
 				tempString[MAX_CHARS] = '\0';
-				coutRed << tempString;
+				coutRef << tempString;
 			}
 			else
 			{
-				coutRed << m_name;
+				coutRef << m_name;
 			}
-			coutRed.fill(' ');
-			coutRed.unsetf(ios::left);
-			coutRed << " ";
-			coutRed << m_row;
-			coutRed << m_letter;
+			coutRef.fill(' ');
+			coutRef.unsetf(ios::left);
+			coutRef << " ";
+			coutRef << m_row;
+			coutRef << m_letter;
 		}
 		else
 		{
-			coutRed.width(40);
-			coutRed.setf(ios::left);
-			coutRed.fill('.');
-			coutRed << m_name;
-			coutRed.fill(' ');
-			coutRed << " ___";
-			coutRed.unsetf(ios::left);
+			coutRef.width(MAX_CHARS);
+			coutRef.setf(ios::left);
+			coutRef.fill('.');
+			coutRef << m_name;
+			coutRef.fill(' ');
+			coutRef << " ___";
+			coutRef.unsetf(ios::left);
 		}
-		return coutRed;
+		return coutRef;
 	}
 
+	//Read passenger information from file
 	istream& Seat::read(istream& cinRef)
 	{
 		reset();
-		char name[70 + 1]{};
-		int row{};
-		char letter{};
+		char name[MAX_NAME_LEN + 1];
+		int row;
+		char letter;
 
-		cinRef.getline(name, 70, ',');
+		cinRef.getline(name, MAX_NAME_LEN, ',');
 		cinRef >> row;
 		cinRef >> letter;
 		cinRef.ignore(1000, '\n');
-		if (cinRef)
+		if (!cinRef.fail())
 		{
 			alAndCp(name);
-			set(row, letter);
+			if (!isEmpty())
+			{
+				set(row, letter);
+			}
 		}		
 		
 		return cinRef;		
--- Workshops/WS04/p1/Seat.h
+++ Workshops/WS04/p1/Seat.h
@@ -1,3 +1,10 @@
+//Name: Carmen Lau
+//Student ID: 166689216
+//Email: clau51@myseneca.ca
+//Date: October 7, 2022
+//Section: NBB
+//I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+
 #ifndef SDDS_SEAT_H_
 #define SDDS_SEAT_H_
 #include <iostream>
@@ -8,22 +15,50 @@ namespace sdds {
 	   char* m_name;
 	   int m_row;
 	   char m_letter;
-
+	   
+	   //Validate correct row numbers and seating letters
 	   bool validate(int row, int letter)const;
+	
+	   //Check if string is valid and if it is, copy string to name
 	   Seat& alAndCp(const char* str);
    public:
+	   //Reset object to safe empty state
 	   Seat& reset();
+	
+	   //Check if object is in empty state
 	   bool isEmpty()const;
+	
+	   //Check if seats are assigned and valid
 	   bool assigned()const;
+
+	   //Default constructor - set object to safe empty state
 	   Seat();
+
+	   //Constructor - deallocate memory and copy string to name
 	   Seat(const char* passengerName);
+	
+	   //Constructor - deallocate memory and copy string to name, then set row and letter
 	   Seat(const char* passengerName, int row, char letter);
+
+	   //Destructor - deallocate memory
 	   ~Seat();
+
+	   //Set row and letter (validate first)
 	   Seat& set(int row, char letter);
+
+	   //Get row
 	   int row()const;
+	
+	   //Get letter
 	   char letter()const;
+
+	   //Get passenger name
 	   const char* passenger() const;
-	   std::ostream& display(std::ostream& coutRed = std::cout)const;
+
+	   //Display passenger and seat information in formatted manner
+	   std::ostream& display(std::ostream& coutRef = std::cout)const;
+
+	   //Read passenger information from file
 	   std::istream& read(std::istream& cinRef = std::cin);
    };
 }
